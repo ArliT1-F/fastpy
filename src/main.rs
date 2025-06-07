99cb2af4f8d26fa287bd62149319c3de22c85f58
Arli <arliturka@gmail.com>
2025-06-07 21:10:09 +0200

    Update .gitignore and refactor main.rs for code parsing functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use tree_sitter::{Parser, Node};
+use tree_sitter_python::language;
+
+fn parse_code(code: &str) -> Option<tree_sitter::Tree> {
+    let mut parser = Parser::new();
+    parser.set_language(language()).ok()?;
+    parser.parse(code, None)
 }
